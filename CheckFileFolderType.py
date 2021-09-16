79a9ee1915fa292af5380c4c848f47458467550d
YeongChai-07 <butchercai@gmail.com>
2021-09-17 00:29:49 +0800

    Aded logic that will invoke relevant command for Unix, Windows and Java based OS to determine the type of file or folder.

--- CheckFileFolderType.py
+++ CheckFileFolderType.py
@@ -1,16 +1,17 @@
-from os import system, path
+import os
 # Gets input from user
 file_folder_name = input("Please enter a name of a file or folder:")
 # Let's check what item type does the file/folder that the user input, belongs to
-if path.isdir(file_folder_name):
+if os.path.isdir(file_folder_name):
     print("This is a directory.\n")
-elif path.isfile(file_folder_name):
+elif os.path.isfile(file_folder_name):
     print("This is a regular file.\n")
-elif path.islink(file_folder_name):
+elif os.path.islink(file_folder_name):
     print("This is a symbolic link.\n")
-elif path.ismount(file_folder_name):
+elif os.path.ismount(file_folder_name):
     print("This is a mount point.\n")
 # Finally displaying the information of the file/folder in long-listing format
-#os_list_dir_command = "ls -lart " # OS-Specific list directory command (Unix)
-os_list_dir_command = "dir " # OS-Specific list directory command (Windows)
-system(os_list_dir_command + file_folder_name) # Invoking the command
+# Using dictionary data structure to store the directory command for individual platform.
+# E.g. posix => Unix OS, nt => Windows OS, java => Java-based OS
+os_list_dir_command_dict = {"posix": "ls -lart ", "nt" : "dir "}
+os.system(os_list_dir_command_dict[os.name] + file_folder_name) # Invoking the command
